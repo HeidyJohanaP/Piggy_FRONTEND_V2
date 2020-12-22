f854220f175978a2cc978e8225cd2c68340458ae
maopinedaangel <74270878+maopinedaangel@users.noreply.github.com>
2020-12-22 18:44:30 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -26,7 +26,7 @@ const router = new vueRouter({
         },
 
         {
-            path: '/usuario/autenticar',
+            path: '/autenticar',
             name: "login",
             component: logIn
         },
